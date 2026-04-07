bf9af93490f489dfdec3e9da6090aa59582f76fc
zilto80-source <zilto80@hotmail.com>
2026-04-07 10:57:31 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'contador-ml-v1';
+const urlsToCache = [
+  'index.html',
+  'leitura.html',
+  'historico.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
